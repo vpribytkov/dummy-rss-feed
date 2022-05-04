06f0e05b428be95be6ccdcc2d9a3f17d7dfe1975
Vasily Pribytkov <vasiliypribytkov@gmail.com>
2022-05-04 22:21:27 +0300

    Add handle to serve static RSS file

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func serveRSS(w http.ResponseWriter, req *http.Request) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		errMsg := fmt.Sprintf("Can't serve rss: %v", err)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(errMsg))
+		return
+	}
+
+	rss, err := os.ReadFile(fmt.Sprintf("%s/sample_rss.xml", pwd))
+	if err != nil {
+		errMsg := fmt.Sprintf("Can't serve rss: %v", err)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(errMsg))
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/xml")
+	w.WriteHeader(http.StatusOK)
+	w.Write(rss)
+}
 
 func main() {
-	fmt.Println("hellow")
+	http.HandleFunc("/rss", serveRSS)
+
+	port := os.Getenv("PORT")
+	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 }
